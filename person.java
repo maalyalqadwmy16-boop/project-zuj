e44b7f55f2afc346b214ea78c5f6ac4666fd03b3
Maaly AL-Qadwmy <maalyalqadwmy16@gmail.com>
2025-11-13 21:45:08 +0300

    Add files via upload

---
+++ person.java
@@ -0,0 +1,17 @@
+
+package w5;
+public class person {
+    
+   String name;
+   int age;
+   
+   public void talk()
+   {
+       System.out.println("Hello my name is "+name);
+   }
+    public void eat()
+   {
+       System.out.println("i am eating......");
+   }
+
+}
---
+++ test.java
@@ -0,0 +1,19 @@
+
+package w5;
+
+
+public class test {
+    public static void main(String[] args) {
+        
+        person p1=new person();
+        p1.name="ata";
+        p1.age=15;
+        
+        person p2=new person();
+        p2.name="jamal";
+        p2.age=20;
+        
+        p1.talk();
+        p2.talk();
+    }
+}
