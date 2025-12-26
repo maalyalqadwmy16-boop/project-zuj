76ab086416561f422faf84e803f9e1232ac17355
Maaly AL-Qadwmy <maalyalqadwmy16@gmail.com>
2025-12-27 01:04:03 +0300

    Add files via upload

---
+++ B.java
@@ -0,0 +1,22 @@
+package Assignment_UN;
+
+public class B extends A {
+
+    @Override
+    public void m3() {
+        System.out.println(" m3 from B");
+    }
+
+    public void m4() {
+        m3();
+        System.out.println(" m4 from B");
+
+    }
+
+    public void m5() {
+        super.m3();
+        System.out.println(" m5 from B");
+
+    }
+
+}
---
+++ TEST1.java
@@ -0,0 +1,22 @@
+package Assignment_UN;
+
+public class TEST1 {
+
+    public static void main(String[] args) {
+
+        A a = new A();
+
+        a.m1();
+        a.m2();
+        a.m3();
+
+        B b = new B();
+
+        b.m1();
+        b.m2();
+        b.m3();
+        b.m4();
+        b.m5();
+
+    }
+}
